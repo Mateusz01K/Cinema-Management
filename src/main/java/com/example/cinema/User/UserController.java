b9e47d0d81da6a63946a14d18c58031a58bd6f77
Mateusz Kusiak <mateusz.kusiak01@gmail.com>
2023-08-02 18:31:12 +0200

    start login

--- src/main/java/com/example/cinema/User/UserController.java
+++ src/main/java/com/example/cinema/User/UserController.java
@@ -1,11 +1,21 @@
 package com.example.cinema.User;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class UserController {
 
     @Autowired
     UserRepository userRepository;
+
+
+    @GetMapping
+    public ModelAndView registration(){
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("/cinema/registration");
+        return mav;
+    }
 }
